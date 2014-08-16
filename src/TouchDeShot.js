d27ff2adfc03bfc890a812298e482a6f64237adc
minimo <MacbookAir@Yoshifumi-no-MacBook-Air.local>
2014-08-16 09:57:44 +0900

    add bgm&se play routine

--- src/TouchDeShot.js
+++ src/TouchDeShot.js
@@ -23,6 +23,11 @@ tds.TouchShooter = tm.createClass({
 
     score: 0,
 
+    bgm: null,
+    bgmIsPlay: false,
+    volumeBGM: 1.0,
+    volumeSE: 1.0,
+
     init: function(id) {
         this.superInit(id);
 
@@ -52,7 +57,63 @@ tds.TouchShooter = tm.createClass({
     exitApp: function() {
         this.stop();
         tm.social.Nineleap.postRanking(this.highScore, "");
-    }
+    },
+
+    playBGM: function(asset) {
+        if (this.bgm) {
+            if (this.bgmIsPlay) {
+                this.bgm.stop();
+                this.bgmIsPlay = false;
+            }
+        }
+        this.bgm = tm.asset.AssetManager.get(asset).clone();
+        if (this.bgm) {
+            this.bgm.loop = true;
+            this.bgm.volume = this.volumeBGM*0.34;
+            this.bgm.play();
+            this.bgmIsPlay = true;
+        }
+        return this.bgm;
+    },
+
+    stopBGM: function() {
+        if (this.bgm) {
+            if (this.bgmIsPlay) {
+                this.bgm.stop();
+                this.bgmIsPlay = false;
+            }
+            this.bgm = null;
+        }
+    },
+
+    pauseBGM: function() {
+        if (this.bgm) {
+            if (this.bgmIsPlay) {
+                this.bgm.pause();
+                this.bgmIsPlay = false;
+            }
+        }
+    },
+
+    resumeBGM: function() {
+        if (this.bgm) {
+            if (!this.bgmIsPlay) {
+                this.bgm.resume();
+                this.bgm.volume = this.volumeBGM*0.34;
+                this.bgmIsPlay = true;
+            }
+        }
+    },
+
+    playSE: function(asset) {
+        var se = tm.asset.AssetManager.get(asset).clone();
+        if (se) {
+            se.loop = false;
+            se.volume = this.volumeSE*0.34;
+            se.play();
+        }
+        return se;
+    },
 });
 
 })();
