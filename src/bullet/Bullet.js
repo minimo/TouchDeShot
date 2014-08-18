f96727c6256de216a9b928b8b2762c3360c87aee
minimo <MacbookAir@Yoshifumi-no-MacBook-Air.local>
2014-08-18 21:07:57 +0900

    bugfix collision

--- src/bullet/Bullet.js
+++ src/bullet/Bullet.js
@@ -178,6 +178,7 @@ tm.define("tds.ShotBullet", {
         for (var i = 0; i < 3; i++) {
             var layer = this.parentScene.layers[s[i]];
             layer.children.each(function(a) {
+                if (a === this.player) return;
                 if (this.parent && a.isCollision && a.isHitElement(this)) {
                     a.damage(this.power);
                     this.vanish();
